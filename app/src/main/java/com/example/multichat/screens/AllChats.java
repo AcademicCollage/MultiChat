1ec6f695f0f2b9e29250ee6a92ca0ce1430576ff
Jeff <jeff@elector.co.il>
2021-05-24 18:03:35 +0300

    ..

--- app/src/main/java/com/example/multichat/screens/AllChats.java
+++ app/src/main/java/com/example/multichat/screens/AllChats.java
@@ -2,7 +2,9 @@ package com.example.multichat.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.multichat.R;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -14,6 +16,26 @@ public class AllChats extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_chats);
+
+    }
+
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.chat_1:
+                moveToNextActivity("class1");
+                break;
+            case R.id.chat_2:
+                moveToNextActivity("class2");
+                break;
+            case R.id.chat_3:
+                moveToNextActivity("class3");
+                break;
+        }
+    }
+    private void moveToNextActivity(String collectionName){
+        Intent intent=new Intent(AllChats.this,ChatTemplate.class);
+        intent.putExtra("collectionName",collectionName);
+        startActivity(intent);
 
     }
 }
--- app/src/main/java/com/example/multichat/screens/ChatTemplate.java
+++ app/src/main/java/com/example/multichat/screens/ChatTemplate.java
@@ -1,26 +1,111 @@
 package com.example.multichat.screens;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.example.multichat.R;
+import com.example.multichat.objects.ChatConversation;
+import com.example.multichat.recycler.ChatRecycler;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import static com.example.multichat.Utils.globalUser;
 
 public class ChatTemplate extends AppCompatActivity {
-
+    RecyclerView recyclerView ;
+    private EditText message;
+    private FirebaseFirestore firebaseFirestore;
+    private ArrayList<ChatConversation>chatConversations;
+    private LottieAnimationView animationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_template);
+        firebaseFirestore=FirebaseFirestore.getInstance();
+
+        initChats();
+        initComponents();
+        liveUpdate();
+    }
+
+    private void liveUpdate() {
+        firebaseFirestore.collection(getIntent().getStringExtra("collectionName"))
+                .document("chats").
+                collection("chats").
+                addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                        if(error!=null){
+                            return;
+                        }
+                        chatConversations= (ArrayList<ChatConversation>) value.toObjects(ChatConversation.class);
+                        setRecyclerView();
+
+                    }
+                });
+    }
+
+    private void initComponents() {
+        TextView textView=findViewById(R.id.name_of_chat);
+        animationView=findViewById(R.id.empty_animation);
+        textView.setText(getIntent().getStringExtra("collectionName"));
+        message=findViewById(R.id.message);
+        Button button=findViewById(R.id.send);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ChatConversation chatConversation=new ChatConversation(globalUser.getUserName(),message.getText().toString(),new Date().toString());
+                firebaseFirestore.collection(getIntent().getStringExtra("collectionName"))
+                        .document("chats").
+                        collection("chats").
+                        add(chatConversation);
+
+            }
+        });
+
+    }
+
+    private void initChats() {
+        firebaseFirestore.
+                collection(getIntent().getStringExtra("collectionName")).
+                document("chats").
+                collection("chats").
+                get().
+                addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        chatConversations= (ArrayList<ChatConversation>) task.getResult().toObjects(ChatConversation.class);
+                        setRecyclerView();
+                        animationView.setVisibility(chatConversations.size()>0? View.GONE:View.VISIBLE);
+
+                    }
+                });
     }
 
     private void setRecyclerView() {
-        recyclerView = findViewById(R.id.recycler_chat);
+        recyclerView = findViewById(R.id.chat_recycler_view);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(layoutManager);
-        RecyclerChat mAdapter = new RecyclerChat(chats);
+        ChatRecycler mAdapter = new ChatRecycler(chatConversations);
         recyclerView.setAdapter(mAdapter);
     }
 }
